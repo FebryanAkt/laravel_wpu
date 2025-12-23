2f7a4df272c7842618eb8812160fc2609340f47c
FebryanAkt <febryan08022006@gmail.com>
2025-12-24 06:06:47 +0700

    struktur folder

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <h1>Home</h1>
+    <h3>Owner: {{$nama }}</h3>
+    <script src="js/script.js"></script>
+    <div class="gambar"><img src="img/gambar1.jpg" alt="gambar1"</div>
+    
+
+</body>
+</html>
